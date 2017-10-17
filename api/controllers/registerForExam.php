6add85924c48f043ae13f34f36901eeb7bfb1765
cwade12c <cwade12c@gmail.com>
2017-10-17 00:21:30 -0700

    Started API validation proxy

--- api/controllers/registerForExam.php
+++ api/controllers/registerForExam.php
@@ -1,15 +1,13 @@
 <?php
 
 require_once('../../config.php');
-enforceAuthentication();
-global $params;
 
 if(isset($_POST['examId']) &&
     isset($_POST['studentId'])
 ) {
   //validate if examId is legit
   //then validate if $param['id'] === $_POST['studentId']
-  $result = registerStudentForExam((int)$_POST['examId'], $params['id']);
+  $result = registerStudentForExam((int)$_POST['examId'], (string)$_POST['studentId']);
   echo $result;
 }
 else {
--- scripts/examInteractions.js
+++ scripts/examInteractions.js
@@ -1,5 +1,5 @@
 function registerStudentForExam(examId, studentId, examObj) {
-  $.post("api/post/registerForExam.php", {examId: examId, studentId: studentId}).done(function (response) {
+  $.post("api/controllers/registerForExam.php", {examId: examId, studentId: studentId}).done(function (response) {
     if(response === '1') {
       notification('Successfully registered for the exam', 'success');
       var elementId = 'exam-' + examId;
@@ -19,7 +19,7 @@ function registerStudentForExam(examId, studentId, examObj) {
 }
 
 function deregisterStudentForExam(examId, studentId, examObj) {
-  $.post("api/post/deregisterForExam.php", {examId: examId, studentId: studentId}).done(function (response) {
+  $.post("api/controllers/deregisterForExam.php", {examId: examId, studentId: studentId}).done(function (response) {
     if(response === '1') {
       notification('Successfully unregistered for the exam', 'warning');
       var elementId = 'exam-' + examId;
@@ -35,5 +35,14 @@ function deregisterStudentForExam(examId, studentId, examObj) {
     else {
       notification('Unable to unregister for the exam');
     }
+  });
+}
+
+function name(name) {
+  var params = {
+    name: name
+  };
+  $.post("api/post.php", {controller: 'name', json: params}).done(function (response) {
+    notification(response, 'success', 'https://google.com', false);
   });
 }

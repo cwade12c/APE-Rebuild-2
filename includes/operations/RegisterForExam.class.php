1122cc52eee85351c3813efbb221310890f6f7e9
cwade12c <cwade12c@gmail.com>
2017-11-04 20:17:38 -0700

    Verify acc belongs to Student

--- includes/operations/RegisterForExam.class.php
+++ includes/operations/RegisterForExam.class.php
@@ -10,11 +10,8 @@
  */
 class RegisterForExam extends Operation
 {
-    private $realAccountID;
-
     function __construct()
     {
-        parent::requireLogin(true);
         parent::setAllowedAccountTypes(array(ACCOUNT_TYPE_STUDENT));
 
         parent::registerExecution(array($this, "registerForExam"));
@@ -30,13 +27,13 @@ class RegisterForExam extends Operation
 
     public function execute(array $args, string $accountID = null)
     {
-        $this->realAccountID = $accountID;
         return parent::execute($args, $accountID);
     }
 
     public static function registerForExam(int $examID, string $studentID)
     {
-        if($studentID != $this->realAccountID) {
+        global $params;
+        if($studentID != $params['id']) {
             throw new Exception("The account you are trying to register does not belong to you!");
         }
 

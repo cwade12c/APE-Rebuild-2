3c0814896ccde440fa6b5f8abdcfe0277693c0fa
Droo-k6 <glitcherfailz@gmail.com>
2017-11-04 23:24:55 -0700

    remove conflict text

--- includes/operations/include.php
+++ includes/operations/include.php
@@ -7,8 +7,6 @@ require_once INCLUDES_PATH . 'operations/StudentState.class.php';
 require_once INCLUDES_PATH . 'operations/RegisterForExam.class.php';
 require_once INCLUDES_PATH . 'operations/RegisterStudentForExam.class.php';
 require_once INCLUDES_PATH . 'operations/UpcomingExams.class.php';
-<<<<<<< HEAD
 require_once INCLUDES_PATH . 'operations/StudentUpcomingExams.class.php';
 require_once INCLUDES_PATH . 'operations/GraderAssignedExams.class.php';
 require_once INCLUDES_PATH . 'operations/MyExams.class.php';
->>>>>>> 2fafc1b22d2b4e4ac687cc9a6cdbcd188c0d2507

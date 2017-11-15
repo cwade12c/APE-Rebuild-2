252e22cc91018050b17bdbf3bdece1aa0adc506d
cwade12c <cwade12c@gmail.com>
2017-11-14 23:35:11 -0800

    Option to disable caching

--- config/general-default.config.php
+++ config/general-default.config.php
@@ -2,11 +2,14 @@
 //General configurations
 
 //Custom site configurations
-DEFINE("SITE_TITLE", "");
+DEFINE("SITE_TITLE", "EWU Advanced Placement Exam");
 
-DEFINE("HOME_PAGE", DOMAIN . "home.php");
+DEFINE("HOME_PAGE", DOMAIN . "home");
 
-DEFINE("AUTH_PAGE", DOMAIN . "login.php");
+DEFINE("AUTH_PAGE", DOMAIN . "login");
+
+//Twig caching configuration
+DEFINE("ENABLE_CACHING", true); //make sure CACHE_PATH is defined in path.config.php
 
 //Account.class configurations
 DEFINE("MINIMUM_NUMBER_OF_ADMINS", 1);
--- includes/templates.inc.php
+++ includes/templates.inc.php
@@ -3,14 +3,14 @@
 global $twig;
 
 $loader = new Twig_Loader_Filesystem(TEMPLATES_PATH);
-if (DEBUG) {
+if (ENABLE_CACHING) {
     $twig = new Twig_Environment($loader, array(
-        'cache' => CACHE_PATH,
-        'auto_reload' => true
+        'cache' => CACHE_PATH
     ));
 }
 else {
     $twig = new Twig_Environment($loader, array(
-        'cache' => CACHE_PATH
+        'cache' => CACHE_PATH,
+        'auto_reload' => true
     ));
 }

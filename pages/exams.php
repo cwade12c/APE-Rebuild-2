5ecd9c08349fa21fe5032e360c30469fbaeaae3a
Curran Higgins <curran@schooldata.net>
2017-11-27 18:33:30 -0800

    Cleaned up Exams page

--- pages/exams.php
+++ pages/exams.php
@@ -3,9 +3,8 @@
 $actions = array();
 
 registerAction($actions, ACTION_CREATE, "Add", "addExam()");
-registerAction($actions, ACTION_GENERIC, "Details", "viewDetails()");
 registerAction($actions, ACTION_UPDATE, "Edit", "editExam()");
-registerAction($actions, ACTION_DELETE, "Delete", "");
+registerAction($actions, ACTION_GENERIC, "Details", "viewDetails()");
 
 $parameters = array(
     "actions" => $actions
